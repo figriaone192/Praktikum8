778f97420cc2be7d5f30a1b252b7b6747b1e371f
figriaone <figriaone@students.amikom.ac.id>
2024-05-30 17:11:19 +0700

    22.11.4694

---
+++ vue-project/src/components/ArtikelHome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img alt="Vue logo" class="logo" src="@/assets/Aime logo.jpg" width="300" height="300" />
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>Ini adalah isi artikel home</p>
+        <p>Ini adalah isi artikel home</p>
+        <p>Ini adalah isi artikel home</p>
+        <p>Ini adalah isi artikel home</p>
+    </div>
+</template>
